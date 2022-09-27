974500131d84404f31e7b6bfaa883495e3b24069
Alex Hinkle <alex.hinkle431@gmail.com>
2022-09-27 11:42:52 -0400

    kc1.py

---
+++ kc1.py
@@ -0,0 +1,12 @@
+'''Knowledge Check 1'''
+
+print('"hello world!"') #A statement that prints "hello world!"
+
+myhero_students = ['Izuku Midoriya','Katsuki Bakugo','Shoto Todoroki'] #A list populated with strings.It is followed by a print statement.
+print(myhero_students[0])
+
+big3_quirks = {'Mirio Togata':'Permeation','Tamaki Amajiki':'Manifest'} #A dictionary populated with two keys and two values. It is followed by a print statement.
+print(big3_quirks['Tamaki Amajiki'])
+
+UA_pros = ('All Might','Endeavor','Eraser Head','Hawks') #A tuple populated with strings. It is followed by a print statement.
+print(UA_pros[2])
