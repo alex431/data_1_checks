b026cdf7a393547056d0d9fdc0b0816be2434f80
Alex Hinkle <alex.hinkle431@gmail.com>
2022-09-27 14:02:35 -0400

    kc1.py

--- kc1.py
+++ kc1.py
@@ -1,12 +1,16 @@
 '''Knowledge Check 1'''
 
-print('"hello world!"') #A statement that prints "hello world!"
+#A statement that prints "hello world!"
+print('"hello world!"')
 
-myhero_students = ['Izuku Midoriya','Katsuki Bakugo','Shoto Todoroki'] #A list populated with strings.It is followed by a print statement.
+#A list populated with strings.It is followed by a print statement.
+myhero_students = ['Izuku Midoriya','Katsuki Bakugo','Shoto Todoroki']
 print(myhero_students[0])
 
-big3_quirks = {'Mirio Togata':'Permeation','Tamaki Amajiki':'Manifest'} #A dictionary populated with two keys and two values. It is followed by a print statement.
+#A dictionary populated with two keys and two values. It is followed by a print statement.
+big3_quirks = {'Mirio Togata':'Permeation','Tamaki Amajiki':'Manifest'}
 print(big3_quirks['Tamaki Amajiki'])
 
-UA_pros = ('All Might','Endeavor','Eraser Head','Hawks') #A tuple populated with strings. It is followed by a print statement.
+#A tuple populated with strings. It is followed by a print statement.
+UA_pros = ('All Might','Endeavor','Eraser Head','Hawks')
 print(UA_pros[2])
